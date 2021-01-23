4a06fcb9206ce554490ab28975e9c06f0153b72c
Igor Cescon de Moura <igorcmgamer@gmail.com>
2021-01-23 08:36:12 -0300

    add camera movement

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform objectToFollow;
+    public float smoothTime = 0.3f;
+
+    Vector3 velocity = Vector3.zero;
+
+    void FixedUpdate()
+    {
+        Vector3 targetPosition = new Vector3(objectToFollow.position.x, objectToFollow.position.y, transform.position.z);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+    }
+}
