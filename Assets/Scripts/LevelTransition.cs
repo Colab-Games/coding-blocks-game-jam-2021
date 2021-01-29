c43fd522e645260ee1c50f71b1ef99f7a4ef5680
Luis Berns <louisuntitled@gmail.com>
2021-01-29 08:40:38 -0300

    ADD: JumpLevel design and gateway between maps

---
+++ Assets/Scripts/LevelTransition.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelTransition : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime = 1f;
+
+    public string sceneName;
+    public string animatorTrigger;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadScene(sceneName);
+            StartCoroutine(AnimateTransition(animatorTrigger));
+        }
+
+    }
+
+    IEnumerator AnimateTransition(string animatorTrigger)
+    {
+        // Play animation
+        transition.SetTrigger(animatorTrigger);
+
+        // Wait
+        yield return new WaitForSeconds(transitionTime);
+    }
+}
