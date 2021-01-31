649ac41608e28b3cb63476aec62d278b177d5de9
Luis Berns <louisuntitled@gmail.com>
2021-01-31 16:25:29 -0300

    ADD: Audio Manager, Main Menu and URP 2D Dynamic Lights

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Slider slider;
+
+    public string sceneName = "GameScene";
+    public float transitionTime = 1f;
+    
+
+    public void PlayGame()
+    {
+        StartCoroutine(LoadingScene(sceneName, transitionTime));
+    }
+
+    IEnumerator LoadingScene(string sceneName, float transitionTime)
+    {
+
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+
+            slider.value = progress;
+
+            yield return null;
+        }
+    }
+}
